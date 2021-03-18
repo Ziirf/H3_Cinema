0ecd37e141100040064d289b78ea0ef7dc290368
Lauge Korsholm <laug0040@elev.tec.dk>
2021-03-18 16:43:53 +0100

    AgeRating, Postcode, Crew Controllers
    CrewController not final, still needs work on the DTO

---
+++ H3_Cinema_Solution/Cinema.Api/Controllers/PostcodesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cinema.Data;
+using Cinema.Domain.Models;
+
+namespace Cinema.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostcodesController : ControllerBase
+    {
+        private readonly CinemaContext _context;
+
+        public PostcodesController(CinemaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Postcodes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Postcode>>> GetPostcodes()
+        {
+            return await _context.Postcodes.ToListAsync();
+        }
+
+        // GET: api/Postcodes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Postcode>> GetPostcode(int id)
+        {
+            var postcode = await _context.Postcodes.FindAsync(id);
+
+            if (postcode == null)
+            {
+                return NotFound();
+            }
+
+            return postcode;
+        }
+
+    }
+}
---
+++ H3_Cinema_Solution/Cinema.Converter/CrewConverter.cs
@@ -0,0 +1,87 @@
+﻿using Cinema.Converters;
+using Cinema.Data;
+using Cinema.Domain.DTOs;
+using Cinema.Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Converter
+{
+    public class CrewConverter : IConverter<Crew, CrewDTO>
+    {
+        private readonly CinemaContext _context;
+        
+        public CrewConverter(CinemaContext context)
+        {
+            _context = context;
+        }
+
+
+        public CrewDTO Convert(Crew crew)
+        {
+            List<MovieCrew> moviecrew = new List<MovieCrew>();
+ 
+            moviecrew = _context.MovieCrew.Where(x => x.Crew.Id == crew.Id)
+                .Include(x => x.Role)
+                .Include(x => x.Movie)
+                .ToList();
+            var movies = new List<Movie>();
+            var roles = new List<string>();
+
+
+            foreach (var role in moviecrew)
+            {
+                if (!roles.Contains(role.Role.Title))
+                {
+                    roles.Add(role.Role.Title);
+                }
+            }
+
+            foreach (var movie in moviecrew)
+            {
+                if (!movies.Contains(movie.Movie))
+                {
+                    movies.Add(movie.Movie);
+                }
+
+            }
+
+            var conver = new MovieConverter(_context);
+
+            var crewDTO = new CrewDTO
+            {
+
+                Id = crew.Id,
+                FirstName = crew.FirstName,
+                LastName = crew.LastName,
+                Birthday = crew.Birthday,
+                ImgUrl = crew.ImgUrl,
+                Description = crew.Description,
+                Roles = roles,
+                StarredIn = movies.Select(x=>conver.Convert(x)).ToList() //Maybe make a StarredInDTO
+
+            };
+
+            return crewDTO;
+        }
+
+        public Crew Convert(CrewDTO crewDTO)
+        {
+            var crew = new Crew
+            {
+                Id = crewDTO.Id,
+                FirstName = crewDTO.FirstName,
+                LastName = crewDTO.LastName,
+                Birthday = crewDTO.Birthday,
+                ImgUrl = crewDTO.ImgUrl,
+                Description = crewDTO.Description
+
+            };
+
+            return crew;
+        }
+
+
+    }
+}
