3478f63d30c1df8b8c755fed4a8aac6231193742
Nicolai Friis <nfriis24@gmail.com>
2021-03-26 12:02:30 +0100

    Changed to XUnit test

---
+++ H3_Cinema_Solution/Cinema.Test/Database/CustomerDb.cs
@@ -0,0 +1,84 @@
+using Cinema.Domain.Models;
+using System;
+using System.Threading.Tasks;
+using Cinema.Api.Controllers;
+using Cinema.Data;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Cinema.Test.Database
+{
+    public class CustomerDb
+    {
+        private CinemaContext _context;
+        public CustomerDb()
+        {
+            var builder = new DbContextOptionsBuilder<CinemaContext>();
+            builder.UseInMemoryDatabase("CinemaDBCustomerTesting");
+            _context = new CinemaContext(builder.Options);
+        }
+
+        [Fact]
+        public async Task CanAddACustomerToDatabase()
+        {
+            var customer = new Customer()
+            {
+                FirstName = "Nicolai",
+                LastName = "Friis"
+            };
+            _context.Add(customer);
+            await _context.SaveChangesAsync();
+            Assert.NotEqual(0, customer.Id);
+        }
+
+        [Fact]
+        public async Task CanReadACustomerFromDatabase()
+        {
+            var customer = new Customer()
+            {
+                FirstName = "Nicolai",
+                LastName = "Friis"
+            };
+            _context.Add(customer);
+            await _context.SaveChangesAsync();
+            Assert.Equal(_context.Customers.Find(customer.Id), customer);
+        }
+
+        [Fact]
+        public async Task CanEditACustomerInDatabase()
+        {
+            var customer = new Customer()
+            {
+                FirstName = "Nicolai",
+                LastName = "Friis"
+            };
+            _context.Add(customer);
+            await _context.SaveChangesAsync();
+            Assert.Equal(customer, _context.Customers.Find(customer.Id));
+
+            customer.FirstName = "Something";
+            customer.LastName = "Else";
+            _context.Entry(customer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            Assert.Equal(customer, _context.Customers.Find(customer.Id));
+        }
+
+        [Fact]
+        public async Task CanDeleteACustomerFromDatabase()
+        {
+            var customer = new Customer()
+            {
+                FirstName = "Nicolai",
+                LastName = "Friis"
+            };
+            _context.Add(customer);
+            await _context.SaveChangesAsync();
+            Assert.Equal(customer, _context.Customers.Find(customer.Id));
+
+            _context.Remove(customer);
+            await _context.SaveChangesAsync();
+            Assert.Null(_context.Customers.Find(customer.Id));
+        }
+    }
+}
