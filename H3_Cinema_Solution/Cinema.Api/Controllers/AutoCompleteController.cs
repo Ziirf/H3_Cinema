65eb421708c99db849624609be38f8c76fedf9f2
Nicolai Friis <nfriis24@gmail.com>
2021-04-07 12:56:58 +0200

    added autoCompleteController

---
+++ H3_Cinema_Solution/Cinema.Api/Controllers/AutoCompleteController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cinema.Domain.Models;
+using Cinema.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoCompleteController : ControllerBase
+    {
+        private readonly CinemaContext _context;
+
+        public AutoCompleteController(CinemaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Movies")]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovieAutoComplete()
+        {
+            var movies = await _context.Movies.Select(x => new {id = x.Id, value = x.Title}).ToListAsync();
+            
+            return Ok(movies);
+        }
+
+        //[HttpGet("Screenings")]
+        //public async Task<ActionResult<IEnumerable<Screening>>> GetScreeningAutoComplete()
+        //{
+        //    var screenings = await _context.Screenings.Select(x => new { id = x.Id, value = x. }).ToListAsync();
+
+        //    return Ok(movies);
+        //}
+
+        [HttpGet("Users")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsersAutoComplete()
+        {
+            // Get all movie and convert them to Movies
+            var users = await _context.Users.Select(x => new { id = x.Id, value = x.Username }).ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("Customers")]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomersAutoComplete()
+        {
+            var customers = await _context.Customers.Select(x => new { id = x.Id, value = $"{x.FirstName} {x.LastName}" }).ToListAsync();
+
+            return Ok(customers);
+        }
+
+        [HttpGet("Theater")]
+        public async Task<ActionResult<IEnumerable<Theater>>> GetTheatersAutoComplete()
+        {
+            var theaters = await _context.Theaters.Select(x => new { id = x.Id, value = $"{x.TheaterName}" }).ToListAsync();
+
+            return Ok(theaters);
+        }
+    }
+}
--- H3_Cinema_Solution/Cinema.Api/Controllers/CustomersController.cs
+++ H3_Cinema_Solution/Cinema.Api/Controllers/CustomersController.cs
@@ -4,7 +4,6 @@ using Cinema.Domain.DTOs;
 using Cinema.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
